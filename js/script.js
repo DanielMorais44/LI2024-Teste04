1cc775f2d74dec989c5bb1e596f99b772fffa507
Daniel Morais <danielmorais44@ua.pt>
2024-12-02 10:43:03 +0000

    5ª tarefa cumprida

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-//ficheiro de script
+//ficheiro de script~
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('contactForm');
+    
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        if (form.checkValidity() === false) {
+            event.stopPropagation();
+        }
+        
+        form.classList.add('was-validated');
+    });
+});
